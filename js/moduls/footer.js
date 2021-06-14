f6a0ea8bf0aa046bb9a7a5fea8e027e2c1e7df14
JalsanSarylov <85345340+AlexxxanderMiron@users.noreply.github.com>
2021-06-14 13:28:55 +0300

    add logo setting

--- js/moduls/footer.js
+++ js/moduls/footer.js
@@ -1,11 +1,11 @@
 $(document).ready(() => {
-    $(".footer").html(`<div class="container">
+  $(".footer").html(`<div class="container">
     <div class="footer-wrapper">
       <div class="footer__left">
-        <a href="#" class="icon__item icon__item__footer"><i class="fa fa-vk" aria-hidden="true"></i></a>
-        <a href="#" class="icon__item icon__item__footer"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-        <a href="#" class="icon__item icon__item__footer"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
-        <a href="#" class="icon__item icon__item__footer"><i class="fa fa-youtube" aria-hidden="true"></i></a>
+      <a href="#" class="icon__item"><i class="fa fa-vk" aria-hidden="true"></i></a>
+      <a href="#" class="icon__item"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+      <a href="#" class="icon__item"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+      <a href="#" class="icon__item"><i class="fa fa-youtube" aria-hidden="true"></i></a>
       </div>
       <p>
         Александр Миронов
